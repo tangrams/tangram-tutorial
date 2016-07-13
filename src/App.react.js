06156dd0037099c6d8a7bd0483495fab0064c41b
Irene Alvarado <ire.alvarado@gmail.com>
2016-07-13 11:46:00 -0400

    new build

--- src/App.react.js
+++ src/App.react.js
@@ -13,16 +13,16 @@ import Tutorial4 from './Components/Tutorial/Tutorial4Filters.react';
 import TutorialConclusion from './Components/Tutorial/TutorialConclusion.react';
 
 let routes = (
-            <Router history={hashHistory}>
-                <Route path="/" component={Main}>
-                    <Route path="/intro" component={Tutorial1}/>
-                    <Route path="/minimum-map/min" component={Tutorial21}/>
-                    <Route path="/minimum-map/data" component={Tutorial22}/>
-                    <Route path="/styling" component={Tutorial3}/>
-                    <Route path="/filters" component={Tutorial4}/>
-                    <Route path="/conclusion" component={TutorialConclusion}/>
-                </Route>
-            </Router>
+    <Router history={hashHistory}>
+        <Route path="/" component={Main}>
+            <Route path="/intro" component={Tutorial1}/>
+            <Route path="/minimum-map/min" component={Tutorial21}/>
+            <Route path="/minimum-map/data" component={Tutorial22}/>
+            <Route path="/styling" component={Tutorial3}/>
+            <Route path="/filters" component={Tutorial4}/>
+            <Route path="/conclusion" component={TutorialConclusion}/>
+        </Route>
+    </Router>
 );
 
 ReactDOM.render(
