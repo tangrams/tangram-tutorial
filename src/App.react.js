2a93f1d04c9bf385035c5d82a419df62d11b6bfb
Irene Alvarado <ire.alvarado@gmail.com>
2016-07-13 11:28:40 -0400

    new build

--- src/App.react.js
+++ src/App.react.js
@@ -2,7 +2,7 @@ import 'babel-polyfill';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory } from 'react-router'
+import { Router, Route, browserHistory, hashHistory} from 'react-router'
 
 import Main from './Components/Main.react';
 import Tutorial1 from './Components/Tutorial/Tutorial1Intro.react';
@@ -13,14 +13,14 @@ import Tutorial4 from './Components/Tutorial/Tutorial4Filters.react';
 import TutorialConclusion from './Components/Tutorial/TutorialConclusion.react';
 
 let routes = (
-            <Router history={browserHistory}>
+            <Router history={hashHistory}>
                 <Route path="/" component={Main}>
-                    <Route path="intro" component={Tutorial1}/>
-                    <Route path="minimum-map/min" component={Tutorial21}/>
-                    <Route path="minimum-map/data" component={Tutorial22}/>
-                    <Route path="styling" component={Tutorial3}/>
-                    <Route path="filters" component={Tutorial4}/>
-                    <Route path="conclusion" component={TutorialConclusion}/>
+                    <Route path="/intro" component={Tutorial1}/>
+                    <Route path="/minimum-map/min" component={Tutorial21}/>
+                    <Route path="/minimum-map/data" component={Tutorial22}/>
+                    <Route path="/styling" component={Tutorial3}/>
+                    <Route path="/filters" component={Tutorial4}/>
+                    <Route path="/conclusion" component={TutorialConclusion}/>
                 </Route>
             </Router>
 );
