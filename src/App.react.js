fad3a8de570a7d1f5066a3386766ffc917260f2e
Irene Alvarado <ire.alvarado@gmail.com>
2016-07-22 12:50:36 -0400

    Storing markdown in a more efficient way

--- src/App.react.js
+++ src/App.react.js
@@ -7,8 +7,7 @@ import { Router, Route, browserHistory, hashHistory } from 'react-router'
 import Main from './Components/Main.react';
 import Tutorial1 from './Components/Tutorial/Tutorial1Intro.react';
 import { Tutorial21, Tutorial22 }  from './Components/Tutorial/Tutorial2Min.react';
-// import Tutorial2_2 from './Components/Tutorial/Tutorial2Min.react';
-import Tutorial3 from './Components/Tutorial/Tutorial3Styling.react';
+import { Tutorial31, Tutorial32, Tutorial33 }  from './Components/Tutorial/Tutorial3Styling.react';
 import Tutorial4 from './Components/Tutorial/Tutorial4Filters.react';
 import TutorialConclusion from './Components/Tutorial/TutorialConclusion.react';
 
@@ -18,7 +17,9 @@ let routes = (
             <Route path="/intro" component={Tutorial1}/>
             <Route path="/minimum-map/min" component={Tutorial21}/>
             <Route path="/minimum-map/data" component={Tutorial22}/>
-            <Route path="/styling" component={Tutorial3}/>
+            <Route path="/styling/lines" component={Tutorial31}/>
+            <Route path="/styling/order" component={Tutorial32}/>
+            <Route path="/styling/labels" component={Tutorial33}/>
             <Route path="/filters" component={Tutorial4}/>
             <Route path="/conclusion" component={TutorialConclusion}/>
         </Route>
