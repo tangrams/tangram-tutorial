5f335203e84bf4a5a120c3a2f0ad28318d76bed1
Irene Alvarado <ire.alvarado@gmail.com>
2016-07-22 16:44:45 -0400

    Adding current build

--- src/App.react.js
+++ src/App.react.js
@@ -7,7 +7,7 @@ import { Router, Route, browserHistory, hashHistory } from 'react-router'
 import Main from './Components/Main.react';
 import Tutorial1 from './Components/Tutorial/Tutorial1Intro.react';
 import { Tutorial21, Tutorial22 }  from './Components/Tutorial/Tutorial2Min.react';
-import { Tutorial31, Tutorial32, Tutorial33 }  from './Components/Tutorial/Tutorial3Styling.react';
+import { Tutorial31, Tutorial32, Tutorial33, Tutorial34 }  from './Components/Tutorial/Tutorial3Styling.react';
 import Tutorial4 from './Components/Tutorial/Tutorial4Filters.react';
 import TutorialConclusion from './Components/Tutorial/TutorialConclusion.react';
 
@@ -20,6 +20,7 @@ let routes = (
             <Route path="/styling/lines" component={Tutorial31}/>
             <Route path="/styling/order" component={Tutorial32}/>
             <Route path="/styling/labels" component={Tutorial33}/>
+            <Route path="/styling/text" component={Tutorial34}/>
             <Route path="/filters" component={Tutorial4}/>
             <Route path="/conclusion" component={TutorialConclusion}/>
         </Route>
--- src/Assets/markdown/styling.md
+++ src/Assets/markdown/styling.md
@@ -63,4 +63,6 @@ If the feature in question is a multipolygon, the centroid point will be added t
 
 [section]
 
+## Customizing Text
+
 Tangram is powerful because it let's you customize the drawing style of your layers to suit your needs. There are many more parameters you can use to customize your labels. Take a look at a few below:
