54af4254163b93b98eb2e205be5db59f6c39c1a8
Irene Alvarado <ire.alvarado@gmail.com>
2016-07-27 18:02:44 -0400

    Adding shorthand files

--- src/Components/Main.react.js
+++ src/Components/Main.react.js
@@ -47,7 +47,8 @@ export default class Main extends React.Component {
                              <ListGroupItem><Link to="/tiles/buildings">Buildings Layer</Link></ListGroupItem>
                              <ListGroupItem><Link to="/tiles/roads">Roads Layer</Link></ListGroupItem>
                              <ListGroupItem><Link to="/tiles/sublayers">Sublayers</Link></ListGroupItem>
-                             <ListGroupItem><Link to="/tiles/layerorder">Layer Order</Link></ListGroupItem>
+                             <ListGroupItem><Link to="/tiles/layerorder">Tip: Layer Order</Link></ListGroupItem>
+                             <ListGroupItem><Link to="/tiles/shorthand">Tip: Syntax Shorthands</Link></ListGroupItem>
                              <ListGroupItem><Link to="/tiles/syntax">URL Syntax</Link></ListGroupItem>
                          </Panel>
                          <Panel collapsible defaultExpanded header="Raster Tiles">
