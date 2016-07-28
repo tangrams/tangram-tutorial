91223cfbab4260f13c229e615c69b8510f942fd3
Irene Alvarado <ire.alvarado@gmail.com>
2016-07-28 15:29:04 -0400

    Tweaking some of the intro text

--- src/Components/Tutorial/Tutorial2Min.react.js
+++ src/Components/Tutorial/Tutorial2Min.react.js
@@ -4,6 +4,7 @@ var React = require('react');
 
 import Row from 'react-bootstrap/lib/Row';
 import TangramPlay from '../TangramPlay.react';
+import Image from 'react-bootstrap/lib/Image';
 import examples from './links';
 
 import min from '../../Assets/markdown/min.md';
@@ -15,6 +16,8 @@ export class Tutorial21 extends React.Component {
     return (
         <Row className="show-grid">
             <div dangerouslySetInnerHTML={{ __html: sections[0] }} />
+            <Image width="100%" src={require('../../Assets/images/playcomment.png')} responsive />
+            <div dangerouslySetInnerHTML={{ __html: sections[1] }}/ >
             <TangramPlay url={examples.min} />
             <div dangerouslySetInnerHTML={{ __html: sections[1] }} />
         </Row>
