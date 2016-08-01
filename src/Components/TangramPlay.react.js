8dc7bd18c7ff5f0703f5b14506612c67b36c0274
Irene Alvarado <ire.alvarado@gmail.com>
2016-08-01 11:14:35 -0400

    Deleting refresh button. It was added to TangramPlay

--- src/Components/TangramPlay.react.js
+++ src/Components/TangramPlay.react.js
@@ -1,47 +1,19 @@
 import 'babel-polyfill';
 import React from 'react';
-import Glyphicon from 'react-bootstrap/lib/Glyphicon';
-import Button from 'react-bootstrap/lib/Button';
 
 /**
  * Represents an icon that receives a 'type' prop indicating how it should look
  * as well as an optional 'active' prop indicating whether icon should be active
  */
 export default class TangramPlay extends React.Component {
-    constructor (props) {
-        super(props);
-        this.state = {
-            buttonRefresh: '',
-            url: this.props.url
-        };
-
-        this.onClick = this.onClick.bind(this);
-    }
-
-    onClick () {
-        this.setState({
-            buttonRefresh: 'bt-spin',
-            url: ''
-        });
-
-        this.setState({
-            buttonRefresh: 'bt-spin',
-            url: this.state.url
-        });
-        // this.forceUpdate();
-        setTimeout(() => { this.setState({ buttonRefresh: '' }); }, 2000);
-    }
-
     /**
      * Official React lifecycle method
      * Called every time state or props are changed
      */
     render () {
-        console.log(this.state.url);
         return (
             <div>
-                <Button className='refresh-button' onClick={this.onClick}><Glyphicon className={this.state.buttonRefresh} glyph="refresh" /></Button>
-                <iframe width="100%" height="700px" frameborder="0" allowfullscreen src={this.state.url}>
+                <iframe width="100%" height="700px" frameborder="0" allowfullscreen src={this.props.url}>
                 </iframe>
             </div>
         );
