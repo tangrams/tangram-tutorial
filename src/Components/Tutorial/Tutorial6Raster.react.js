0b58dd621de86fed41e9083c5cc99747688e74d6
Irene Alvarado <ire.alvarado@gmail.com>
2016-08-02 17:23:36 -0400

    Fixing typo in intro yaml

--- src/Components/Tutorial/Tutorial6Raster.react.js
+++ src/Components/Tutorial/Tutorial6Raster.react.js
@@ -5,6 +5,8 @@ var React = require('react');
 import Row from 'react-bootstrap/lib/Row';
 import Image from 'react-bootstrap/lib/Image';
 import TangramPlay from '../TangramPlay.react';
+import Section from './Section.react';
+
 import examples from './links';
 
 import raster from '../../Assets/markdown/raster.md';
@@ -13,14 +15,16 @@ var sections = raster.split('[section]');
 
 export class Tutorial61 extends React.Component {
   render () {
+      console.log(this.props.route.prev);
+      console.log(this.props.route.next);
     return (
-        <Row className="show-grid">
+        <Section prev={this.props.route.prev} next={this.props.route.next}>
             <div dangerouslySetInnerHTML={{ __html: sections[0] }} />
             <TangramPlay url={examples.raster} />
             <div dangerouslySetInnerHTML={{ __html: sections[1] }} />
             <TangramPlay url={examples.raster2} />
             <div dangerouslySetInnerHTML={{ __html: sections[2] }} />
             <TangramPlay url={examples.raster3} />
-        </Row>
+        </Section>
         )}
 }
