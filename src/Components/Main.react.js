5c0a56809b22c30365e1d462153ab309ec087f82
Irene Alvarado <ire.alvarado@gmail.com>
2016-08-18 18:20:03 -0400

    Adding collapsible panels

--- src/Components/Main.react.js
+++ src/Components/Main.react.js
@@ -5,6 +5,7 @@ import Grid from 'react-bootstrap/lib/Grid';
 import Col from 'react-bootstrap/lib/Col';
 import Row from 'react-bootstrap/lib/Row';
 import Panel from 'react-bootstrap/lib/Panel';
+import PanelGroup from 'react-bootstrap/lib/PanelGroup';
 import ListGroup from 'react-bootstrap/lib/ListGroup';
 import ListGroupItem from 'react-bootstrap/lib/ListGroupItem';
 import Image from 'react-bootstrap/lib/Image';
@@ -12,6 +13,22 @@ import Image from 'react-bootstrap/lib/Image';
 import content from '../App.react';
 
 export default class Main extends React.Component {
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            activeKey: '0'
+        };
+
+        this.handleSelect = this.handleSelect.bind(this);
+    }
+
+    handleSelect(activeKey) {
+        console.log("being selected");
+       this.setState({ activeKey });
+     }
+
+
   render () {
       require('../Assets/css/bootstrap.css');
       require('../Assets/css/style.scss');
@@ -19,41 +36,32 @@ export default class Main extends React.Component {
      return (
       <Grid className='grid-container'>
         <Row>
-            <Col md={2} className='sidebar'>
+            <Col sm={2} md={2} className='sidebar'>
                 <Row className='sidebar-title'>
                     <Image className='sidebar-title-logo' src={require('../Assets/images/logo.png')} responsive />
                 </Row>
                 <Row className='sidebar-toc'>
-                    <ListGroup fill>
+                    <PanelGroup activeKey={this.state.activeKey} onSelect={this.handleSelect} accordion>
                         {
                             // Iterating through each main section
                             content.map(function(c, i) {
-                                let divStyle = {
-                                      backgroundColor: 'white'
-                                };
-
                                 // Iterating through each sub section
-                                let subsections = c.sections.map(function(s, j) {
-                                    return <ListGroupItem key={j} style=
-                                        {(() => {
-                                            if(this.props.location.pathname == s.path) {
-                                                return divStyle
-                                            }
-                                        })()}
-                                    ><Link to={s.path}>{s.name}</Link></ListGroupItem>
-                                }, this)
+                                let subsections = c.sections.map((s, j) => {
+                                    return <ListGroupItem key={j}>
+                                                <Link to={s.path}>{s.name}</Link>
+                                           </ListGroupItem>})
 
-                                let mainsection = <Panel collapsible key={i} header={c.title} >
+                                let mainsection = <Panel collapsible key={i} eventKey={i.toString()} header={c.title} >
                                                     {subsections}
                                                   </Panel>;
 
                                 return mainsection ;
                             }, this)
                         }
-                    </ListGroup>
+                    </PanelGroup>
                 </Row>
             </Col>
-            <Col md={10} className='content'>
+            <Col sm={10} md={10} className='content'>
                 <Row className='menu-bar'>
                     <div className='title'>Tangram Tutorial</div>
                 </Row>
